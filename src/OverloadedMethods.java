38ba3a979061d36b8c269f634a7d67972c29691a
Manikanta <manikanta3517@hmail.com>
2019-09-07 18:57:05 -0400

    example-1

---
+++ src/OverloadedMethods.java
@@ -0,0 +1,39 @@
+public class OverloadedMethods
+{
+    public static void main(String[] args)
+    {
+        Integer integer[] = {1,2,3,4,5,6};
+        Character chars[]={'a','b','c'};
+        Double dou[] = {1.1,2.2,3.3,4.4};
+
+        System.out.println("Array of integers");
+        printarray(integer);
+        System.out.println("Array of chars");
+        printarray(chars);
+        System.out.println("Array of dou");
+        printarray(dou);
+
+
+
+    }
+    private static void printarray(Integer[] integer) {
+        for(Integer i : integer){
+            System.out.printf("%s ",i) ;
+        }
+        System.out.println();
+    }
+    private static void printarray(Character[] chars) {
+        for(Character i : chars){
+            System.out.printf("%s ",i) ;
+        }
+        System.out.println();
+
+    }
+    private static void printarray(Double[] dou) {
+        for(double i : dou){
+            System.out.printf("%s ",i) ;
+        }
+        System.out.println();
+
+    }
+}
