bd6ac52b8b1e549418e65ae601a03c3f116bcf7c
Manikanta <manikanta3517@hmail.com>
2019-09-15 21:27:36 -0400

    20.3 upperbound and method call explicit casting completed

--- src/OverloadedMethods.java
+++ src/OverloadedMethods.java
@@ -2,69 +2,64 @@ public class OverloadedMethods
 {
     public static void main(String[] args)
     {
-        Integer integer[] = {1,2,3,4,5,6};
-        Character chars[]={'a','b','c'};
-        Double dou[] = {1.1,2.2,3.3,4.4};
+        System.out.printf("%d %n",
+        max(1,2,3));
+        System.out.printf("%f %n",
+        max(1.1,2.2,3.3));
+        System.out.printf("%s",
+        max("mani","nani","pani"));
 
-        System.out.println("Array of integers");
-        printarray(integer);
-        System.out.println("Array of chars");
-        printarray(chars);
-        System.out.println("Array of dou");
-        printarray(dou);
     }
-//    private static void printarray(Integer[] integer) {
-//        for(Integer i : integer){
-//            System.out.printf("%s ",i) ;
-//        }
-//        System.out.println();
-//    }
-//    private static void printarray(Character[] chars) {
-//        for(Character i : chars){
-//            System.out.printf("%s ",i) ;
-//        }
-//        System.out.println();
-//
-//    }
-//    private static void printarray(Double[] dou) {
-//        for(double i : dou){
-//            System.out.printf("%s ",i) ;
-//        }
-//        System.out.println();
-//
-//    }
 
-    /**
-     * Advantage of generic method is Compilation time Type Safe hence prevents runtime errors.
+    /***
+     * Specifying a type parameters upper-bound:
+     * Only objects of the class that implements the interface Comparable<T> can be used in this Method
      *
+     * When the code is translated into byte code:
+     * private static  Comparable  max( Comparable x,  Comparable y,  Comparable z) {
+     *         Comparable maximum = x;
+     *         if(y.compareTo(maximum) > 0)
+     *         {
+     *             maximum = y ;
+     *         }
+     *         if(z.compareTo(maximum) > 0)
+     *         {
+     *             maximum = z;
+     *         }
+     *          return maximum;
+     *     }
      *
-     * All Generic Methods type parameter section that is precedes return type
-     * 1>type parameter section can contain one or more type variables separated by commas
-     * 2>type variables can used as return type , parameters and local-variables.
-     * 3>type variables can represent only reference types not primitive types.
-     * 4>type variable  can appear only once in  type parameter section but can appear more that once in method parameter list.
-     *
-     *
-     * What is happening here?
-     * 1.Compiler encounters printarray(integer) then locate printarray(Integer[] integer) it is not there.
-     * so next compiler locate for generic method named printarray with single array array parameter and uses typed variable
-     * 2.Compilier also determines if the operation in method body can be performed on elements that are being passed as parameter.
+     * Actually all the typed parameters are replaced with UPPER-BOUND.
      *
      *
      */
 
-    public static <T> void printarray(T[] inputarray){
-        for(T element : inputarray){
-            System.out.printf("%s",element);
+    private static <T extends Comparable<T>> T max( T x,  T y,  T z) {
+        T maximum = x;
+        if(y.compareTo(maximum) > 0)
+        {
+            maximum = y ;
         }
-
-        /**
-         * After copilation in byte code above code is represented as
-         *  public static void printarray(object [] inputarray){
-         *         for(object element : inputarray){
-         *             System.out.printf("%s",element);
-         *         }
-         */
+        if(z.compareTo(maximum) > 0)
+        {
+            maximum = z;
+        }
+         return maximum;
     }
+
+    /**
+     * Whats happening?
+     *
+     * when compilier encounters the method call max it looks max() with 3 arguments of type int, but no such method
+     * next, compiler looks for generic method. as generic method arguments must be of REFERENCE TYPE so compiler
+     * autoboxes as Integer Type and passes to max() and also Class Integer implements Comparable<Integer> so the
+     * compareTo(maximum) can be performed {{compiler checks all the operation performed inside the method}}.
+     *
+     * Here return type is of Comparable but method call is expecting back the type Integer. So during byte code
+     * translation explicit cast operation is placed infrount of the method call:
+     *
+     * (Integer) max(1,2,3)).
+     */
+
 
 }
