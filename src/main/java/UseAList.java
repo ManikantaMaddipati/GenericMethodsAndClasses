133a3b1c14761d4655f43dffac136792b447238f
manikanta <manikanta3517@gmail.com>
2020-07-24 18:14:58 -0400

    Observation3/4 : Now compilation error is thrown

--- src/main/java/UseAList.java
+++ src/main/java/UseAList.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 public class UseAList {
     public static void main(String[] args) {
-        List names = new ArrayList();
+        //By telling compiler that we are going to add only string
+        List<String> names = new ArrayList();
         names.add("KING");
         names.add("Queen");
         //Observation1 : This List can take anything;
-        names.add(LocalDate.now());
+        //Observation3 : Now compilation error is thrown
+        //names.add(LocalDate.now());
 
         //Observation 2 : Explicit casting is needed.
-        String s = (String) names.get(0);
+        //Observation 4 : and no explicit casting is needed.
+        String s = names.get(0);
 
         for (Object o: names) {
             System.out.println(o);
