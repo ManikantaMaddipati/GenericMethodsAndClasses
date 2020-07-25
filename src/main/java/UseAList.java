3f1cc6a80e840714873bd7603a8a6d89ca9e5065
manikanta <manikanta3517@gmail.com>
2020-07-25 11:18:36 -0400

    Observation 5:Generic Type is Missing in parameter section but list names is accepting LocalDate

--- src/main/java/UseAList.java
+++ src/main/java/UseAList.java
@@ -11,15 +11,24 @@ public class UseAList {
         //Observation1 : This List can take anything;
         //Observation3 : Now compilation error is thrown
         //names.add(LocalDate.now());
+        breakList(names);
 
         //Observation 2 : Explicit casting is needed.
         //Observation 4 : and no explicit casting is needed.
         String s = names.get(0); // Observation 5 : implicit casting is present
 
+        List<LocalDate> localDates =  new ArrayList();
+        localDates.add(LocalDate.now());
+
         for (Object o: names) {
             System.out.println(o);
         }
 
+    }
+
+    //Observation 5:Generic Type is Missing in parameter section but list names is accepting LocalDate
+    private static void breakList(List names) {
+        names.add(LocalDate.now());
     }
 
 
